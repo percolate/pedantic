64ff5a08129c5c7f864e3c6b883d86f50f9a0b3b
Julien Funk <closingiris@gmail.com>
2017-05-10 13:20:00 -0400

    minor doc cleanup

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-# Testing Multiple Python Versions with Tox
-# https://ben.fogbutter.com/2016/02/20/testing-multiple-python-versions-on-circleci.html
 python -m unittest discover --start-directory ./tests/unit/
