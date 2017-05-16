e4363c0731505044c57a60bd864101e49f45618e
Julien Funk <closingiris@gmail.com>
2017-05-16 14:10:56 -0400

    version 0.0.2

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@
 FROM python:3.6.1-alpine
 LABEL Description="This image is used to start the local Pendantic service." Author="Julien Funk" Version="0.0.1"
 
+ADD VERSION .
 ADD pedantic/ requirements.txt pedantic.py /
 ADD pedantic/ /pedantic
 RUN pip install -r requirements.txt
---
+++ release.sh
@@ -0,0 +1,30 @@
+set -ex
+
+# docker hub username
+USERNAME=julienfunk
+# image name
+IMAGE=pedantic
+
+# ensure we're up to date
+git pull
+
+# bumps the version
+docker run --rm -v "$PWD":/app treeder/bump patch
+
+version=`cat VERSION`
+echo "version: ${version}"
+
+# run build
+docker build -t ${USERNAME}/${IMAGE} .
+
+# tag it
+git add -A
+git commit -m "version ${version}"
+git tag -a "${version}" -m "version ${version}"
+git push
+git push --tags
+docker tag ${USERNAME}/${IMAGE}:latest ${USERNAME}/${IMAGE}:${version}
+
+# push it
+docker push ${USERNAME}/${IMAGE}:latest
+docker push ${USERNAME}/${IMAGE}:${version}
